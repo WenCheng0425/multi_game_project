2562de83f8c31511795fd7f841ff0226d76c63b6
Vincent <fofolongkk@gmail.com>
2025-12-17 09:50:12 +0800

    Fix: connect 2 users success

--- client.c
+++ client.c
@@ -5,12 +5,25 @@
 
 // IP 只有 Client 需要知道，所以留在這裡
 // 【重要】：當你搬到第二台電腦時，這裡要改成第一台電腦(Server)的真實 IP
-#define SERVER_IP "127.0.0.1"
+// 我們不再這裡寫死 IP，改用變數接收
+// #define SERVER_IP "127.0.0.1"
 
-int main() {
+int main(int argc, char *argv[]) {
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
+    char *target_ip;
+
+    // --- 這裡加入了判斷邏輯 ---
+    if (argc < 2) {
+        // 如果使用者沒輸入 IP，預設連本機 (開發用)
+        printf("未輸入 IP，預設連線到 127.0.0.1\n");
+        target_ip = "127.0.0.1";
+    } else {
+        // 如果使用者有輸入，就用輸入的 IP (連線用)
+        target_ip = argv[1];
+    }
+    // ------------------------
 
     // 1. 建立 Socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -22,20 +35,23 @@ int main() {
     serv_addr.sin_port = htons(DEFAULT_PORT);
 
     // 轉換 IP 地址 (連線到本機 127.0.0.1)
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
+    if (inet_pton(AF_INET, target_ip, &serv_addr.sin_addr) <= 0) {
+        printf("\n無效的 IP 位址: %s \n", target_ip);
         return -1;
     }
+
+    printf("嘗試連線到 %s:%d ...\n", target_ip, DEFAULT_PORT);
 
     // 2. 連線
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
+        printf("\n連線失敗 (Connection Failed)\n");
+        printf("請檢查: 1. Server開了嗎? 2. IP對嗎? 3. 防火牆關了嗎?\n");
         return -1;
     }
 
-    printf("已連線到 Server! (直接打字並按 Enter 即可發送)\n");
+    printf("連線成功！(直接打字並按 Enter 即可發送)\n");
 
-    fd_set readfds; // 檔案描述符集合，用來管理「誰有資料進來」
+    fd_set readfds;
 
     while (1) {
         FD_ZERO(&readfds);
